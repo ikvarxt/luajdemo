29dbb4e13abadca4b508ed69853556a7b99933d0
ikvarxt <leonekola@outlook.com>
2025-05-26 18:09:58 +0800

    feat: lua network based on okhttp, get

---
+++ app/src/main/java/com/example/luajdemo/lualib/NetworkLib.kt
@@ -0,0 +1,68 @@
+package com.example.luajdemo.lualib
+
+import com.example.luajdemo.helper.luaValue
+import okhttp3.Headers
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.logging.HttpLoggingInterceptor
+import org.luaj.vm2.LuaValue
+import org.luaj.vm2.Varargs
+import org.luaj.vm2.lib.TwoArgFunction
+import org.luaj.vm2.lib.VarArgFunction
+
+class NetworkLib : TwoArgFunction() {
+
+    private lateinit var client: OkHttpClient
+
+    override fun call(modname: LuaValue, env: LuaValue): LuaValue {
+        initializeOkhttpClient()
+        val lib = LuaValue.tableOf()
+
+        lib.set("get", Get())
+
+        env.set("network", lib)
+        env.get("package").get("loaded").set("network", lib)
+        return env
+    }
+
+    private fun initializeOkhttpClient() {
+        client = OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .build()
+    }
+
+    inner class Get : VarArgFunction() {
+
+        override fun invoke(args: Varargs): LuaValue {
+            val url = args.arg(1).tojstring()
+            val params = args.arg(2)
+
+            val req = Request.Builder()
+                .url(url)
+                .method("GET", null)
+                .headers(params.parseHeaders())
+                .build()
+
+            client.newCall(req).execute().use { response ->
+                return response.body?.string().luaValue()
+            }
+        }
+    }
+
+    private fun LuaValue.parseHeaders(): Headers {
+        val headersBuilder = Headers.Builder()
+        val paramsTable = if (istable()) checktable() else tableOf()
+        val headers = paramsTable.get("headers")
+        if (headers.istable()) {
+            val table = headers.checktable()
+            table.keys()
+                .map { it.tojstring() }
+                .forEach { key ->
+                    headersBuilder.add(key, table.get(key).tojstring())
+                }
+        }
+        return headersBuilder.build()
+    }
+
+
+}
---
+++ app/src/test/java/com/example/luajdemo/lualib/NetworkLibTest.kt
@@ -0,0 +1,47 @@
+package com.example.luajdemo.lualib
+
+import com.example.luajdemo.BaseEngineTest
+import com.example.luajdemo.LuaEngine
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class NetworkLibTest : BaseEngineTest() {
+
+    override fun onInitialize(engine: LuaEngine) {
+        engine.loadLib(NetworkLib())
+    }
+
+    @Test
+    fun `check network lib exists`() {
+        val result = run("return network")
+        assertTrue(result.istable())
+        assertTrue(result.get("get").isfunction())
+    }
+
+    @Test
+    fun `simple get request`() {
+        val res = run(
+            """
+            return network.get('https://httpbin.org/get')
+            """.trimIndent()
+        )
+        assertTrue(res.checkjstring().contains("https://httpbin.org/get"))
+    }
+
+    @Test
+    fun `get request with headers`() {
+        val res = run(
+            """
+            local json = require('json')
+            local r = network.get('https://httpbin.org/get', { headers = { a = 'a', b = 'b' }})
+            return json.decode(r)
+            """.trimIndent()
+        )
+        assertTrue(res.istable(), "table")
+        val table = res.checktable()
+        val headers = table["headers"]
+        assertEquals("a", headers["A"].checkjstring())
+        assertEquals("b", headers["B"].checkjstring())
+    }
+}
